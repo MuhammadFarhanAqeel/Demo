221a86e0b44c37e5c52f89817172aecf07fb18b4
Muhammad Farhan <farhan.aqeel@hotmail.com>
2015-03-09 00:22:54 -0700

    Project
    Complete solution

---
+++ Assets/Scripts/HeartBounce.js
@@ -0,0 +1,85 @@
+﻿
+
+var hasLost:boolean = false;
+var numHits:int = 0;
+var hitCount:GUIText;
+var velocityWasStored = false;
+var storedVelocity : Vector3;
+var bestScore:int = 0;
+var lastScore:int = 0;
+
+
+function OnCollisionEnter(col:Collision) 
+{
+if (col.gameObject.tag == "tray" )
+	{
+	Debug.Log("yeah!! hit tray");
+		if(!velocityWasStored)
+		{
+			storedVelocity = rigidbody.velocity;
+			velocityWasStored = true;
+		}
+		if(rigidbody.velocity.y >1 )
+		{
+		numHits ++;
+		}
+		rigidbody.velocity.y = storedVelocity.y;
+	}
+}
+
+
+function Start () {
+
+}
+
+
+
+function Update () 
+{
+
+var str:String = "";
+	if(!hasLost)
+	{
+		str = numHits.ToString();
+	} 
+	else
+	{
+		str = "Hits: " +numHits.ToString() + "\n Your best:" +bestScore;
+		if(bestScore > lastScore) str+= "\nNEW Record!!";
+	}
+	
+	hitCount.text = str;
+	if(transform.position.y < -3 )
+	{
+		if(!hasLost)
+		{
+		hasLost = true;
+		lastBest = bestScore;
+			if(numHits > bestScore)
+			{
+			bestScore = numHits;
+			}
+		}
+	}
+}
+
+
+function OnGUI()
+{
+	if (hasLost)
+	{
+	var buttonW:int = 150;
+	var buttonH:int =80;
+	var halfScreenW:float = Screen.width/2;
+	var halfbuttonW:float = buttonW/2;
+	
+	
+		if (GUI.Button(Rect(halfScreenW - halfbuttonW, Screen.height*0.8,buttonW, buttonH), "Play Again"))
+		{
+		numHits=0;
+		hasLost = false;
+		transform.position = Vector3(-0.6,2,1.37);
+		rigidbody.velocity = Vector3(0,0,0);
+		}
+	}
+}
---
+++ Assets/Scripts/Mousefollow.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+var smooth = 5.0;
+var tiltAngle = 30.0;
+
+function Start () {
+
+}
+
+function Update () {
+var tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle * -2;
+var tiltAroundX = Input.GetAxis("Vertical") * tiltAngle * -2 ;
+var halfW : float = Screen.width/2;
+var halfH:float = Screen.height/3;
+var forEu= new Vector3(tiltAroundX,0,tiltAroundZ);
+
+transform.position.y = (Input.mousePosition.y -halfH )/ halfH;
+transform.position.x = (Input.mousePosition.x - halfW)/halfW;
+
+//smoothly tilts the transform in to the target position
+
+var target = Quaternion.Euler(forEu);
+
+transform.rotation = Quaternion.Slerp(transform.rotation,target,Time.deltaTime*smooth);
+
+}
